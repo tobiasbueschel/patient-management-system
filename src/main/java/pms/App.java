7d6851766da4e7df6d36d6a5b5e768b212cddadf
Tobias Büschel <tobias.bueschel@gmail.com>
2020-04-30 00:32:59 +0800

    test: add a simple AppTest class

--- src/main/java/pms/App.java
+++ src/main/java/pms/App.java
@@ -11,14 +11,12 @@ public class App {
 
   public static void main(String[] args) {
 
-    EventQueue.invokeLater(new Runnable() {
-      public void run() {
-        try {
-          Login frame = new Login();
-          frame.setVisible(true);
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+    EventQueue.invokeLater(() -> {
+      try {
+        Login frame = new Login();
+        frame.setVisible(true);
+      } catch (Exception e) {
+        e.printStackTrace();
       }
     });
   }
---
+++ src/test/java/pms/AppTest.java
@@ -0,0 +1,15 @@
+package pms;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    @DisplayName("App Class Test")
+    void testMainMethod() {
+      assertNotNull("Add meaningful tests");
+    }
+}
